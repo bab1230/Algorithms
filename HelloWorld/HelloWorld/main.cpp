56130ac015728c71bc3ca527871e55547b139fde
Braden Wallace <bradenwallace@Bradens-MacBook-Pro.local>
2018-08-22 18:23:39 -0500

    Excecuteable created

--- HelloWorld/HelloWorld/main.cpp
+++ HelloWorld/HelloWorld/main.cpp
@@ -1,15 +1,7 @@
-//
-//  main.cpp
-//  HelloWorld
-//
-//  Created by Braden Wallace on 8/22/18.
-//  Copyright © 2018 Braden Wallace. All rights reserved.
-//
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
 }
